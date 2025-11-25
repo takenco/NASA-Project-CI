f0ddb817db35af6107147337f30134e753cf9453
Frank_takenco <frank.ndjekoua@takenco.com>
2025-11-25 09:21:41 +0100

    jest@29 installedand jest arg added in server folder

--- server/src/models/planets.mongo.js
+++ server/src/models/planets.mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const planetSchema = new mongoose.Schema({
 
-    keplerName: { type: String, required: true}
+    keplerName: { type: String, required: false}
 });
 
 module.exports = mongoose.model('Planet', planetSchema);
