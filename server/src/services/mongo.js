f0e1b88fbe6efd69bac34832ba38ba93c2590972
Frank_takenco <frank.ndjekoua@takenco.com>
2025-11-25 09:34:56 +0100

    jest@29 installed and jest timeout removed 1.1

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL;
 
-// mongoose.connection.once('open', () => {
-//     console.log('MongoDB connection ready!');
-// });
-mongoose.connection.on('open', () => {
+mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
 });
+
 mongoose.connection.on('error', (err) => {
     console.error(err);
 })
